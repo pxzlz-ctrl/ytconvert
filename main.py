33f42a50d6e91489fd916ee80fb28f613b1a0ccb
Pxzlz <91290196+pxzlz-ctrl@users.noreply.github.com>
2023-11-30 01:03:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from your_app_module import app
+from app import app
 
 if __name__ == "__main__":
-    import os
-    import sys
     from gunicorn.app.wsgiapp import WSGIApplication
 
     sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
