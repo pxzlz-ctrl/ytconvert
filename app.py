a2763b60e3cc880577210776b61e6ff05f6f2e62
Pxzlz <91290196+pxzlz-ctrl@users.noreply.github.com>
2023-11-30 01:11:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 from pytube import YouTube
 from pytube.exceptions import PytubeError
 from flask import Flask, render_template, request, send_file
+
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
@@ -11,14 +12,15 @@ def serveMain():
     return render_template('index.html')
 
 def clean_filename(title):
-  clean_title = title.translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation))).replace('  ', ' ').strip()
-  return clean_title
+    clean_title = title.translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation))).replace('  ', ' ').strip()
+    return clean_title
 
 @app.route('/download', methods=['POST'])
 def download_youtube():
     yt_url = request.form['ytUrl']
     format = request.form['format']
     file_path = None
+    
     try:
         yt = YouTube(yt_url, on_progress_callback=None)
         if format == 'mp4':
@@ -29,16 +31,18 @@ def download_youtube():
             file_extension = 'mp3'
         
         filename = clean_filename(yt.title) + '.' + file_extension
+        tmp_file_path = os.path.join('/tmp', filename)
+
         if format == 'mp4':
-            video_stream.download(output_path='static/videos', filename=filename)
+            video_stream.download(output_path='/tmp', filename=filename)
         elif format == 'mp3':
-            audio_stream.download(output_path='static/videos', filename=filename)
-            
-        file_path = os.path.join('static', 'videos', filename)
-        return send_file(file_path, as_attachment=True)
+            audio_stream.download(output_path='/tmp', filename=filename)
+        
+        return send_file(tmp_file_path, as_attachment=True)
     except Exception as e:
         # Handle the exception
         return "An error occurred: " + str(e)
     finally:
-        if file_path is not None:
-            os.remove(file_path)
+        # Clean up local file in /tmp
+        if tmp_file_path is not None:
+            os.remove(tmp_file_path)
