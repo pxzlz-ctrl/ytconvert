7006814aa21580d1ac27506a2d59352c01a0a7a1
Pxzlz <91290196+pxzlz-ctrl@users.noreply.github.com>
2023-12-12 23:34:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,56 +1,48 @@
 import os
 import string
-import asyncio
+import requests
 from pytube import YouTube
-from flask import Flask, render_template, request, jsonify
 from pytube.exceptions import PytubeError
+from flask import Flask, render_template, request, send_file
 
 app = Flask(__name__, static_url_path='/static')
 
-async def download_youtube_video_async(yt_url, file_format):
-    try:
-        youtube_video = YouTube(yt_url, on_progress_callback=None)
-
-        format_mapping = {
-            'mp4': ('get_highest_resolution', 'mp4'),
-            'mp3': ('get_audio_only', 'mp3')
-        }
-
-        stream_method, file_extension = format_mapping[file_format]
-        stream = getattr(youtube_video.streams, stream_method)()
-
-        filename = clean_filename(youtube_video.title) + '.' + file_extension
-        tmp_file_path = os.path.join('/tmp', filename)
-
-        stream.download(output_path='/tmp', filename=filename)
-
-        return tmp_file_path
-    except PytubeError as e:
-        raise Exception(f"Pytube error occurred: {e}")
-    except Exception as e:
-        raise Exception(f"An error occurred: {e}")
+@app.route('/')
+def serveMain():
+    return render_template('index.html')
 
 def clean_filename(title):
     clean_title = title.translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation))).replace('  ', ' ').strip()
     return clean_title
 
-async def background_task(yt_url, file_format):
-    tmp_file_path = await download_youtube_video_async(yt_url, file_format)
-    # Perform any additional processing or notifications here
-    return tmp_file_path
+@app.route('/download', methods=['POST'])
+def download_youtube():
+    yt_url = request.form['ytUrl']
+    format = request.form['format']
+    tmp_file_path = None
+    
+    try:
+        yt = YouTube(yt_url, on_progress_callback=None)
+        if format == 'mp4':
+            video_stream = yt.streams.get_highest_resolution()
+            file_extension = 'mp4'
+        elif format == 'mp3':
+            audio_stream = yt.streams.get_audio_only(subtype='mp4')
+            file_extension = 'mp3'
+        
+        filename = clean_filename(yt.title) + '.' + file_extension
+        tmp_file_path = os.path.join('/tmp', filename)
 
-def handler(event, context):
-    if event['httpMethod'] == 'POST':
-        data = json.loads(event['body'])
-        yt_url = data.get('ytUrl')
-        file_format = data.get('format')
-
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-        result = loop.run_until_complete(background_task(yt_url, file_format))
-        loop.close()
-
-        return {
-            'statusCode': 200,
-            'body': json.dumps({'status': 'Download complete.', 'file_path': result})
-        }
+        if format == 'mp4':
+            video_stream.download(output_path='/tmp', filename=filename)
+        elif format == 'mp3':
+            audio_stream.download(output_path='/tmp', filename=filename)
+        
+        return send_file(tmp_file_path, as_attachment=True)
+    except Exception as e:
+        # Handle the exception
+        return "An error occurred: " + str(e)
+    finally:
+        # Clean up local file in /tmp
+        if tmp_file_path is not None:
+            os.remove(tmp_file_path)
