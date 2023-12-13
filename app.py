26c50b27c3fa397deb6f2ca487cdc0eb6053fc7b
Pxzlz <91290196+pxzlz-ctrl@users.noreply.github.com>
2023-12-12 23:25:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
 import os
 import string
-import requests
 from pytube import YouTube
-from pytube.exceptions import PytubeError
 from flask import Flask, render_template, request, send_file
+from pytube.exceptions import PytubeError
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
-def serveMain():
+def serve_main():
     return render_template('index.html')
 
 def clean_filename(title):
@@ -18,31 +17,30 @@ def clean_filename(title):
 @app.route('/download', methods=['POST'])
 def download_youtube():
     yt_url = request.form['ytUrl']
-    format = request.form['format']
+    file_format = request.form['format']
     tmp_file_path = None
     
     try:
-        yt = YouTube(yt_url, on_progress_callback=None)
-        if format == 'mp4':
-            video_stream = yt.streams.get_highest_resolution()
-            file_extension = 'mp4'
-        elif format == 'mp3':
-            audio_stream = yt.streams.get_audio_only(subtype='mp4')
-            file_extension = 'mp3'
+        youtube_video = YouTube(yt_url, on_progress_callback=None)
         
-        filename = clean_filename(yt.title) + '.' + file_extension
+        format_mapping = {
+            'mp4': ('get_highest_resolution', 'mp4'),
+            'mp3': ('get_audio_only', 'mp3')
+        }
+
+        stream_method, file_extension = format_mapping[file_format]
+        stream = getattr(youtube_video.streams, stream_method)()
+
+        filename = clean_filename(youtube_video.title) + '.' + file_extension
         tmp_file_path = os.path.join('/tmp', filename)
 
-        if format == 'mp4':
-            video_stream.download(output_path='/tmp', filename=filename)
-        elif format == 'mp3':
-            audio_stream.download(output_path='/tmp', filename=filename)
+        stream.download(output_path='/tmp', filename=filename)
         
         return send_file(tmp_file_path, as_attachment=True)
+    except PytubeError as e:
+        return f"Pytube error occurred: {e}"
     except Exception as e:
-        # Handle the exception
-        return "An error occurred: " + str(e)
+        return f"An error occurred: {e}"
     finally:
-        # Clean up local file in /tmp
         if tmp_file_path is not None:
             os.remove(tmp_file_path)
