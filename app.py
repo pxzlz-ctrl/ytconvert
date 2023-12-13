84875741de2764b57a600ab578a192c1e487e7fa
Pxzlz <91290196+pxzlz-ctrl@users.noreply.github.com>
2023-12-12 23:31:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 import os
 import string
+import asyncio
 from pytube import YouTube
-from flask import Flask, render_template, request, send_file
+from flask import Flask, render_template, request, jsonify
 from pytube.exceptions import PytubeError
 
 app = Flask(__name__, static_url_path='/static')
 
-@app.route('/')
-def serve_main():
-    return render_template('index.html')
-
-def clean_filename(title):
-    clean_title = title.translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation))).replace('  ', ' ').strip()
-    return clean_title
-
-def download_youtube_video(yt_url, file_format):
+async def download_youtube_video_async(yt_url, file_format):
     try:
         youtube_video = YouTube(yt_url, on_progress_callback=None)
-        
+
         format_mapping = {
             'mp4': ('get_highest_resolution', 'mp4'),
             'mp3': ('get_audio_only', 'mp3')
@@ -37,17 +30,25 @@ def download_youtube_video(yt_url, file_format):
     except Exception as e:
         raise Exception(f"An error occurred: {e}")
 
+def clean_filename(title):
+    clean_title = title.translate(str.maketrans(string.punctuation, ' ' * len(string.punctuation))).replace('  ', ' ').strip()
+    return clean_title
+
+async def background_task(yt_url, file_format):
+    tmp_file_path = await download_youtube_video_async(yt_url, file_format)
+    # Perform any additional processing or notifications here
+    return tmp_file_path
+
+@app.route('/')
+def serve_main():
+    return render_template('index.html')
+
 @app.route('/download', methods=['POST'])
-def download_youtube():
+def initiate_download():
     yt_url = request.form.get('ytUrl')
     file_format = request.form.get('format')
-    tmp_file_path = None
-    
-    try:
-        tmp_file_path = download_youtube_video(yt_url, file_format)
-        return send_file(tmp_file_path, as_attachment=True)
-    except Exception as e:
-        return str(e)
-    finally:
-        if tmp_file_path is not None:
-            os.remove(tmp_file_path)
+
+    # Start the background download task
+    asyncio.create_task(background_task(yt_url, file_format))
+
+    return jsonify({'status': 'Download initiated. You will be notified when it\'s ready.'})
